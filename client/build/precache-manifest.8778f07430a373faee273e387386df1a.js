ebf9cdc9491df1d6f5970573a110752864fd8c02
melosdauti7@gmail.com <melosdauti7@gmail.com>
2020-10-08 14:01:21 +0200

    fdf

---
+++ client/build/precache-manifest.8778f07430a373faee273e387386df1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df6960807f1bd767b08b2f85cda82a1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09c63b650f26a2b8a039",
+    "url": "/static/css/main.4835ebf5.chunk.css"
+  },
+  {
+    "revision": "0b9a16a961f09ad5faed",
+    "url": "/static/js/2.0a3f5fbc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0a3f5fbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09c63b650f26a2b8a039",
+    "url": "/static/js/main.e3014c7b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
