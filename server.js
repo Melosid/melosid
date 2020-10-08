5ab17729b1561721aa3f4ceccac21247986c7bb2
melosdauti7@gmail.com <melosdauti7@gmail.com>
2020-10-08 15:09:27 +0200

    specifikimi i versionit te nodde

--- server.js
+++ server.js
@@ -9,18 +9,18 @@ const PORT = process.env.PORT || 8080;
 
 const routes = require("./routes/api");
 
-// mongoose.connect(
-//   process.env.MONGODB_URI ||
-//     "mongodb+srv://melos:melos@cluster0.v0kzk.mongodb.net/bllahbllah?retryWrites=true&w=majority",
-//   {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   }
-// );
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb+srv://melos:melos@cluster0.v0kzk.mongodb.net/bllahbllah?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
-// mongoose.connection.on("connected", () => {
-//   console.log("Mongoose is connected");
-// });
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose is connected");
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
